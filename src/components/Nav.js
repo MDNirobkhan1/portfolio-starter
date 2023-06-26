3c50a8ae66b240cf825643c92d04191e05619cb4
MDNirobkhan1 <nirob937612@gmail.com>
2023-06-27 00:43:45 +0600

    nav added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import {BiHomeAlt, BiUser} from 'react-icons/bi'
+import {BsClipboard2Data, BsBriefcase, BsChatSquare} from 'react-icons/bs';
+import {Link} from 'react-scroll'
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <div className='container mx-auto'>
+      <div className='w-full bg-black/20 h-[80px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50'>
+        <Link classID='cursor-pointer w-[60px] flex items-center justify-center'><BiHomeAlt /></Link>
+        <Link classID='cursor-pointer w-[60px] flex items-center justify-center'><BiUser /></Link>
+        <Link classID='cursor-pointer w-[60px] flex items-center justify-center'><BsClipboard2Data /></Link>
+        <Link classID='cursor-pointer w-[60px] flex items-center justify-center'><BsBriefcase /></Link>
+        <Link classID='cursor-pointer w-[60px] flex items-center justify-center'><BsChatSquare /></Link>
+      </div>
+    </div>
+  </nav>;
 };
 
 export default Nav;
