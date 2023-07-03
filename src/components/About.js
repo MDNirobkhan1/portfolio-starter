9f42156505f0d4499fedfa3c646bbce1a442df39
MDNirobkhan1 <nirob937612@gmail.com>
2023-07-03 23:07:20 +0600

    added

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ const About = () => {
           <p>Experience is the name everyone gives to their mistakes.</p>
           <h3>
             <p>My Intro</p>
-            Hey, This is Akib Rayhan, I am 23 years old.I live in Dhaka in Bangladesh. I completed my 4 years diploma course in computer science & engineering from Comilla Polytechnic Institute in 2021. I am currently learning web development. I am a front-end web developer. I have worked on many of my projects. Every project is different and works front-end, back-end, and design done by me. I request you to see my projects.
+            Hey, This is Mohiuddin, I am 23 years old.I live in Dhaka in Bangladesh. I completed my 4 years diploma course in computer science & engineering from Comilla Polytechnic Institute in 2021. I am currently learning web development. I am a front-end web developer. I have worked on many of my projects. Every project is different and works front-end, back-end, and design done by me. I request you to see my projects.
           </h3>
         </motion.div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import logo from '../assets/pic.jpg'
-import { Link } from 'react-scroll';
+
 
 const Header = () => {
   return <header className='py-8'>
@@ -15,7 +15,7 @@ const Header = () => {
           </a>
         </div>
 
-        <a className='btn btn-sm' href="https://drive.google.com/uc?id=1tibh6T0TItsaOdKaoO-oz3Yc338jjuT4&export=download" referrerPolicy='no-referrer' target='_blank' download rel="noreferrer">Resume</a>
+        <a className='btn btn-sm' href="https://drive.google.com/uc?id=1tibh6T0TItsaOdKaoO-oz3Yc338jjuT4&export=download" referrerPolicy='no-referrer' target='_blank' download rel="noreferrer">My Resume</a>
       </div>
     </div>
   </header>;
