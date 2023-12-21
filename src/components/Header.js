b7ccf9b77b13446b75f4dca00f57a3e2757a43d2
MDNirobkhan1 <nirob937612@gmail.com>
2023-12-21 14:28:14 +0600

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import logo from '../assets/pic.jpg'
-
+import React from "react";
+import logo from "../assets/pic.jpg";
 
 const Header = () => {
-  return <header className='py-8'>
-    <div className='container mx-auto'>
-      <div className='flex justify-between items-center sm:gap-3'>
-        <div>
-          <a href="#" className='text-3xl flex'>
-            <img className='w-[60px] h-[60px] rounded-full' src={logo} alt="" />
-            <span className='ml-2 text-4xl text-gradient'> PORTFOLIO</span>
-          </a>
-        </div>
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center sm:gap-3">
+          <div>
+            <a href="#" className="text-3xl flex">
+              <img
+                className="w-[60px] h-[60px] rounded-full"
+                src={logo}
+                alt=""
+              />
+              <span className="ml-2 text-4xl text-gradient"> PORTFOLIO</span>
+            </a>
+          </div>
 
-        <div>
-        <a className='btn btn-sm' href="https://drive.google.com/uc?id=1tibh6T0TItsaOdKaoO-oz3Yc338jjuT4&export=download" referrerPolicy='no-referrer' target='_blank' download rel="noreferrer">My Resume</a>
+          <div>
+            <a
+              className="btn btn-sm"
+              href="https://drive.google.com/file/d/1N4wWS0BT5kBXvHHvVBFNTo60PLs6XClH/view?usp=download"
+              referrerPolicy="no-referrer"
+              target="_blank"
+              download
+              rel="noreferrer"
+            >
+              My Resume
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
